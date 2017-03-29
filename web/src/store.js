e6fc71707f6ca9b4b3b0d69c1d7715a54f2a335c
Lawrence Romano <l.j.romano@gmail.com>
2017-03-29 16:56:50 -0400

    Started creation of store.  Added cases for set_name, set_price, and clear_item.  Also wrapped App in Provider

---
+++ web/src/store.js
@@ -0,0 +1,28 @@
+import { createStore } from 'redux'
+import { set, lensProp } from 'ramda'
+
+const initialState = {
+  name: "",
+  price: ""
+}
+
+const item = (state=initialState, action) => {
+  switch (action.type) {
+    case 'SET_NAME':
+      return set(lensProp('name'), action.payload, state)
+
+    case 'SET_PRICE':
+      return set(lensProp('price'), action.payload, state)
+
+    case 'CLEAR_ITEM':
+      return {}
+
+    default:
+      return state
+  }
+}
+
+const store = createStore(item)
+
+
+export default store
