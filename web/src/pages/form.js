a6d7ec37a207ee2d610bdff2b44be414008d482e
Lawrence Romano <l.j.romano@gmail.com>
2017-03-29 16:24:35 -0400

    Mapped State and Actions to Props.  Built set_name, set_price and clear_item

--- web/src/pages/form.js
+++ web/src/pages/form.js
@@ -1,31 +1,57 @@
 import React from 'react'
 import InputField from '../components/input'
 import { connect } from 'react-redux'
+import fetch from 'isomorphic-fetch'
 
+const postItem = (item) => {
+  fetch(`http://localhost:5000/widgets`, {
+    headers: {
+      "Content-Type": "application/json"
+    },
+    method: "POST",
+    body: JSON.stringify(item)
+  })
+}
 
 const AddItem = (props) => {
   return (
-    <form className="pa4 black-80">
-      <InputField 
+    <form onSubmit={props.submit(props.item)} className="pa4 black-80">
+      <InputField
         title='Name'
-      
+        onChange={props.changeName}
       />
-      <InputField 
+      <InputField
         title='Price'
-        
+        onChange={props.changePrice}
       />
     </form>
   )
 }
 
-const mapStateToProps = (state => state)
+const mapStateToProps = (state) => ({
+  item: state.item
+})
 
-const mapActionsToProps = (dispatch) => {
-  changeName: 
-  changePrice: 
-  
-}
+const mapActionsToProps = (dispatch) => ({
+  changeName: (e) => dispatch({
+    type: 'SET_NAME', payload: e.target.value
+  }),
+  changePrice: (e) => dispatch({
+    type: 'SET_PRICE', payload: e.target.value
+  }),
+  submit: (item) => (e) => {
+    e.preventDefault()
+    postItem(item)
+    .then(res => res.json())
+    .then(res => {
+      dispatch({
+        type: 'CLEAR_ITEM'
+      })
+    })
+  }
+
+})
 
 const connector = connect(mapStateToProps, mapActionsToProps)
 
-export default AddItem
+export default connector(AddItem)
