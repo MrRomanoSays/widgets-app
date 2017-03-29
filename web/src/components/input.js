0f6645ee57398c61a4fff264dca0ed08feef93f4
Stanley Cruse <stanley.cruse3@gmail.com>
2017-03-29 15:34:59 -0400

    added input to form and store

--- web/src/components/input.js
+++ web/src/components/input.js
@@ -3,11 +3,22 @@ import React from 'react'
 const InputField = (props) => {
   return (
     <div className="measure">
-      <label htmlFor="name" className="f6 b db mb2">Name <span className="normal black-60">(optional)</span></label>
-      <input id="name" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" aria-describedby="name-desc" />
-      <small id="name-desc" className="f6 black-60 db mb2">Helper text for the form control.</small>
+      <label htmlFor="name" className="f6 b db mb2">{props.title}<span className="normal black-60">{props.optional}</span></label>
+      <input id="name" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" aria-describedby="name-desc" 
+        value={props.value}
+        onChange={props.onChange}
+      />
+    <small id="name-desc" className="f6 black-60 db mb2">{props.desc}</small>
     </div>
   )
+}
+
+InputField.propTypes = {
+  title: React.PropTypes.string,
+  desc: React.PropTypes.string,
+  optional: React.PropTypes.bool,
+  onChange: React.PropTypes.func,
+  value: React.PropTypes.string
 }
 
 export default InputField
--- web/src/pages/form.js
+++ web/src/pages/form.js
@@ -1,16 +1,31 @@
 import React from 'react'
+import InputField from '../components/input'
+import { connect } from 'react-redux'
 
 
 const AddItem = (props) => {
   return (
     <form className="pa4 black-80">
-        <div className="measure">
-          <label htmlFor="name" className="f6 b db mb2">Name <span className="normal black-60">(optional)</span></label>
-          <input id="name" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" aria-describedby="name-desc" />
-          <small id="name-desc" className="f6 black-60 db mb2">Helper text for the form control.</small>
-        </div>
-      </form>
+      <InputField 
+        title='Name'
+      
+      />
+      <InputField 
+        title='Price'
+        
+      />
+    </form>
   )
 }
+
+const mapStateToProps = (state => state)
+
+const mapActionsToProps = (dispatch) => {
+  changeName: 
+  changePrice: 
+  
+}
+
+const connector = connect(mapStateToProps, mapActionsToProps)
 
 export default AddItem
